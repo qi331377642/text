6c7dc7f08d6b727abcb6dfa2df38e2246e30533e
qi331377642 <53906351+qi331377642@users.noreply.github.com>
2019-10-16 16:05:36 +0800

    Update TextApplicationTests.java

--- src/test/java/com/lipengqi/TextApplicationTests.java
+++ src/test/java/com/lipengqi/TextApplicationTests.java
@@ -14,6 +14,7 @@ public class TextApplicationTests {
 
         System.out.println("-------------1-----------");
          System.out.println("-------------2-----------");
+        System.out.println("-------------5-----------");
 
     }
 
