ffff3909b8b7405e344293a03a11a5d18b28180f
qi331377642 <53906351+qi331377642@users.noreply.github.com>
2019-10-16 16:03:43 +0800

    Update TextApplicationTests.java

--- src/test/java/com/lipengqi/TextApplicationTests.java
+++ src/test/java/com/lipengqi/TextApplicationTests.java
@@ -13,6 +13,7 @@ public class TextApplicationTests {
     public void contextLoads() {
 
         System.out.println("-------------1-----------");
+         System.out.println("-------------2-----------");
 
     }
 
